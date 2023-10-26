15023aed4829117e7a8de6de6379b8f885da8b7e
mathferreira01 <ferreiramatheus124@gmail.com>
2023-10-26 17:13:02 -0300

    Build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} text-gray-900`}>
         <Theme>
           <NavBar />
-          <main>{children}</main>
+          <main className='p-5'>{children}</main>
         </Theme>
       </body>
     </html>
