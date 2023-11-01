cc016818665f527fa155dd7a6c5241b68a0a180a
mathferreira01 <ferreiramatheus124@gmail.com>
2023-11-01 16:18:45 -0300

    Secure the application

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,14 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from '@/prisma/client'
 import { IssueSchema } from "../../createIssueSchema";
-
-export async function getServerSideProps() {
-  const issues = prisma.issue.findMany()
-
-  return NextResponse.json(issues, {status: 200})
-}
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request:NextRequest) {
+  const session = await getServerSession(authOptions)
+  if(!session) return NextResponse.json({}, {status: 401})
+
   const body = await request.json()
   const validation = IssueSchema.safeParse(body)
   
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -9,12 +9,16 @@ import ReactMarkdown from 'react-markdown';
 import IssueDetails from './IssueDetails';
 import EditIssueButton from './EditIssueButton';
 import DeleteIssueButton from './DeleteIssueButton';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOptions';
 
 interface Props {
   params: {id: string}
 }
 
 const IssueDetailPage = async ({params}: Props) => {
+  const session = await getServerSession(authOptions)
+
   const issue = await prisma.issue.findUnique({
     where: {id: parseInt(params.id)}
   })
@@ -28,10 +32,12 @@ const IssueDetailPage = async ({params}: Props) => {
         <Box className='md:col-span-4'>
           <IssueDetails issue={issue}/>
         </Box>
-        <Box>
-          <EditIssueButton issueId={issue.id}/>
-          <DeleteIssueButton issueId={issue.id}/>
-        </Box>
+        {session && (
+          <Box>
+            <EditIssueButton issueId={issue.id}/>
+            <DeleteIssueButton issueId={issue.id}/>
+          </Box>
+        )}
       </Grid>   
   )
 }
